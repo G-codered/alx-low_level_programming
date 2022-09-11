44a563152c681ff958f552a7d2a34da9e921f684
G-codered <ngbejume5@gmail.com>
2022-09-11 20:43:19 +0100

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print string in the put function
+ * main - print string in the printf function
  *
  * Description: using the main function
  * this program prints "with proper grammar, but the outcome is a piece of art,
