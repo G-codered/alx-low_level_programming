826d91038ddb8a235076bdcc6a37536c4545d498
G-codered <ngbejume5@gmail.com>
2022-09-12 18:22:39 +0100

    print negative, positive and zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h> 
+#include <stdio.h>
+
+/*Betty style doc for function main goes there*/
 /**
  * main - print the string in the printf function
  *
