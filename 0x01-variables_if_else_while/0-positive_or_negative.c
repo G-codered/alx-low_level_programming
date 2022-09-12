593d8238ef9375383aae0aebe266a0373afcbddb
G-codered <ngbejume5@gmail.com>
2022-09-12 16:27:58 +0100

    print negative, positive and zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - priht the string in the printf function
+ *
+ * Description: using the main function
+ * the program prints a random variable weather positive or negative
+ * Return: (0)
+ */
+
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if n > 0
+	{
+		printf("%ld is positive\n".n)
+	}
+	else if n = 0
+	{
+		printf("%ld is zero\n".n)
+	}
+	else if n < 0
+	{
+		printf("%ld is negative\n".n)
+	}
+	return (0);
+}
+
