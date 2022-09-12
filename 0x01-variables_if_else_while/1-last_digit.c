d2e75043dde2801fb7c868c17baf3bc1462fb79a
G-codered <ngbejume5@gmail.com>
2022-09-12 19:36:07 +0100

    the last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print the string in the printf function
+ *
+ * Description: the function in the main, this program prints lesser than, greater than, equal to and not equal to
+ * Return: (0)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 5)
+		printf("Last digit of %d Is %d and is greater than 5\n", n)
+	else if (n == 0)
+		printf("Last digit of %d Is %d and is 0\n", n)
+	else if (0<n<6)
+		printf("Last digit of %d Is %d and is less than 6 and not 0\n", n)
+	return (0);
+}
