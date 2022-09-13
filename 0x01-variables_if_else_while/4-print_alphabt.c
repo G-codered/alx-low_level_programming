ba2d1a766a79ebb5f3e8698b55d576bf728d82d7
G-codered <ngbejume5@gmail.com>
2022-09-13 03:49:24 +0100

    putchar lowercase except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+                                                  /**
+ * main - print the string in the putchar function
+ *
+ * Descripton: the main function
+ * this program prints both lowercase except e and q alphabets   
+ * Return: (0)
+ */
+
+int main(void)
+
+{                                                         char ch;                                  
+        for (ch = 'a'; ch ch <= 'z'; ch++)
+	if (ch != 'e' && ch != 'q')
+        putchar (ch);
+
+	putchar ('\n')
+	return (0)
+}
