25d2b0ab724f533b51e5249f239f37cddb4766a0
G-codered <ngbejume5@gmail.com>
2022-09-13 05:10:03 +0100

    print lowercase alphabets from z - a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <studio.h>
+
+/**
+ * main - print the string in putchar function
+ * 
+ * Description: using the main function
+ * this program prints lowercase alphabets in reverse
+ * Return: (0)
+ */
+
+int main(void)
+	
+{
+	char c;
+
+	if(c = z; c >= a; c++)
+	putchar(c);
+
+	putchar(\n);
+	return (0);
+}
