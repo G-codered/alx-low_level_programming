33750ba30159aaba3fa8184bd39d7f46093861f2
G-codered <ngbejume5@gmail.com>
2022-09-13 01:20:39 +0100

    play with alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the putchar function
+ *
+ * Description: the main function, this program prints lowercase alphabets
+ * Return: (0)
+ */
+
+int main(void)
+
+{
+	for (ch = 'a'; ch < 'f'; ch++)
+		putchar (ch);
+	return (0)
+}
