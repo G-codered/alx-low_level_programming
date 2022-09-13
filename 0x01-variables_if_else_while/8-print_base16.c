974ae4b269bdc76e3209eb92a9f8e0373cc37c09
G-codered <ngbejume5@gmail.com>
2022-09-13 05:43:15 +0100

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print string in putchar function
+ *
+ * Descriiption: using main function
+ * this program prints numerals
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+	char c;
+	if (c = '0'; c <= '9'; c++)
+	printchar(c);
+
+	if (c = 'a'; c <= 'f'; c++)
+	printchar(c);
+
+	printchar('\n');
+	return (0);
+}
+
