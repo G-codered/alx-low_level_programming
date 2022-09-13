c1974e22329ab0d4f33bcbe183f126295fb673a6
G-codered <ngbejume5@gmail.com>
2022-09-13 04:48:48 +0100

    putchar numerals

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print the string in putchar function
+ * 
+ * Description: using the main function
+ * this program prints number 0 - 9
+ * 
+ * Return: (0)
+ */
+
+int main(void)
+
+{
+	int i;
+
+	if (i = 0 ; i < 10 ; i++)
+	putchar("%i", i);
+
+	putchar("\n");
+	return (0)
+}
